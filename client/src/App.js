5d48dc74eda17bce5fc96b0c5257c6554668d23e
alocam30 <99086842+alocam30@users.noreply.github.com>
2022-11-27 17:57:31 -0800

    add components

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import withContext from "./Context";
 
-function App() {
+//------------------COMPONENTS---------------//
+import Header from "./components/Header";
+import Courses from "./components/Courses";
+import UserSignIn from "./components/UserSignIn";
+import UserSignUp from "./components/UserSignUp";
+import CourseDetail from "./components/CourseDetail";
+import UserSignOut from "./components/UserSignOut";
+import CreateCourse from "./components/CreateCourse";
+import UpdateCourse from './Components/UpdateCourse';
+
+const HeaderWithContext = withContext(Header);
+const CoursesWithContext = withContext(Courses);
+const UserSignInWithContext = withContext(UserSignIn);
+const CourseDetailWithContext = withContext(CourseDetail);
+const UserSignOutWithContext = withContext(UserSignOut);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <HeaderWithContext />
+
+      <Routes>
+        <Route path="/" element={<CoursesWithContext />} />
+        <Route path="courses/:id" element={<CourseDetailWithContext />} />
+        <Route path="/signin" element={<UserSignInWithContext />} />
+        <Route path="/signout" element={<UserSignOutWithContext />} />
+      </Routes>
+    </React.Fragment>
   );
-}
+};
+
 
 export default App;
